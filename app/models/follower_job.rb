a30fe70e42dfee528a6f8df67693dfe356fec83b
Matt Manning <matt.manning@gmail.com>
2011-08-10 00:21:31 -0400

    Get my twitter account out of there.

--- app/models/follower_job.rb
+++ app/models/follower_job.rb
@@ -1,6 +1,6 @@
 class FollowerJob
   def perform
-    followers = Twitter.followers('mattmanning').users
+    followers = Twitter.followers.users
     followers.each do |follower|
       Delayed::Job.enqueue AnnoyerJob.new(follower.screen_name)
     end
