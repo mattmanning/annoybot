3d09c0db8fd54c61bbccf1efb63fd0b81b3cd296
Matt Manning <matt.manning@gmail.com>
2011-08-09 22:30:29 -0400

    Tweaks to clock.

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,5 +1,5 @@
-require 'config/boot'
-require 'config/environment'
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
