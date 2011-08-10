a823ef3427754d15fbd8bbab84122b0e8ef6b659
Matt Manning <matt.manning@gmail.com>
2011-08-09 23:21:41 -0400

    Have the follower job queue AnnoyerJobs

--- app/models/follower_job.rb
+++ app/models/follower_job.rb
@@ -1,5 +1,8 @@
 class FollowerJob
   def perform
-    followers = Twitter.followers('mattmanning').users.map(&:screen_name)
+    followers = Twitter.followers('mattmanning').users
+    followers.each do |follower|
+      Delayed::Job.enqueue AnnoyerJob.new(follower.screen_name)
+    end
   end
 end
